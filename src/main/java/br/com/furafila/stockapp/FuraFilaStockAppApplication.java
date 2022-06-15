9d6d795fb2cc627fad8a6197131747961fab00ec
Wellington Pires <wellington.goncalves@live.com>
2022-06-16 00:02:15 +0100

    Initial commit

---
+++ src/main/java/br/com/furafila/stockapp/FuraFilaStockAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.furafila.stockapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuraFilaStockAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuraFilaStockAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/furafila/stockapp/FuraFilaStockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.furafila.stockapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuraFilaStockAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
